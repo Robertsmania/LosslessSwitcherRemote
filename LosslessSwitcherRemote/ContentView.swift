391f0e41863d8761f52cb0f839418157dbffbdc6
Kris Roberts <kris@robertsmania.com>
2023-04-06 22:18:11 -0700

    Initial Commit

---
+++ LosslessSwitcherRemote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LosslessSwitcherRemote
+//
+//  Created by Kris Roberts on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LosslessSwitcherRemote/LosslessSwitcherRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LosslessSwitcherRemoteApp.swift
+//  LosslessSwitcherRemote
+//
+//  Created by Kris Roberts on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LosslessSwitcherRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
